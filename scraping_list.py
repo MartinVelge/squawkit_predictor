ebf128d74e9cb22429c1adf75678a4cc033f284b
Martin Velge <martinvelge@MacBook-Air-de-Martin.local>
2023-03-22 12:16:55 +0100

    json as output for reporting

--- scraping_list.py
+++ scraping_list.py
@@ -23,40 +23,34 @@ def get_airline_tables(airline_tuple):
     last_date, airline_url, country, airline_name = airline_tuple
     try:
         list_tables = pd.read_html(airline_url)
-        salary = list_tables[3].iloc[2:4,1].tolist()
-        last_update = list_tables[3].iloc[2:4,2].tolist()
-        return airline_url, airline_name, salary, last_date, country, last_update
+        CaptMax, CaptMin = list_tables[3].iloc[2:4,1].tolist()
+        DateCaptMax, DateCaptMin = list_tables[3].iloc[2:4,2].tolist()
+        return airline_url, airline_name, CaptMax, CaptMin, last_date, country, DateCaptMax, DateCaptMin
     except Exception as e:
-        try:
-            return e, airline_url, airline_name, list_tables[3], last_date, country, list_tables[3]
-        except NameError:
-            return e, airline_url, airline_name, None, last_date, country, None
+            return e, airline_url, airline_name
 
 def extract_airlines_from_jobs(jobs):
     return set((job['last_date'], job['airline_url'], job['country'], job['airline_name']) for job in jobs)
 
-def clean_salaries(salaries):
-    df_salary = pd.DataFrame([salary for salary in salaries if len(salary) == 6])
-    df_salary.columns = ['URL', 'AirlineName', 'CaptMax', 'CaptMin', 'DateCaptMax', 'DateCaptMin']
-    print("df_salary:", df_salary) 
 
+def clean_salaries(salaries):
+    df_salary = pd.DataFrame([salary for salary in salaries if len(salary) == 8])
+    df_salary.columns = ['URL', 'AirlineName', 'CaptMax', 'CaptMin', 'LastUpdate', 'Country', 'DateCaptMax', 'DateCaptMin']
+    
     # DF Stack on Top/Base. Requirement for ML model (Olivier)
-    df_stack = (df_salary.set_index(('URL', 'AirlineName', 'DateCaptMax', 'DateCaptMin'))
-    .rename_axis(['Top/Base'], axis=1)
-    .stack(dropna=False) # Put True to remove NaNs
-    .reset_index())
-    df_stack.columns = ['URL', 'AirlineName', 'DateCaptMax', 'DateCaptMin', 'Top/Base', 'Salary']
+    df_stack = (df_salary.set_index(['URL', 'AirlineName', 'LastUpdate', 'Country', 'DateCaptMax', 'DateCaptMin'])
+        .rename_axis(['Top/Base'], axis=1)
+        .stack(dropna=False) # Put True to remove NaNs
+        .reset_index())
+    df_stack.columns = ['URL', 'AirlineName', 'LastUpdate', 'Country', 'DateCaptMax', 'DateCaptMin', 'Top/Base', 'Salary']
 
     # Year extraction
     df_stack['Date_reworked'] = np.where(df_stack['Top/Base'] == 'CaptMax', df_stack['DateCaptMax'], df_stack['DateCaptMin'])
     df_stack['Date_reworked'] = pd.to_datetime(df_stack['Date_reworked'])
     df_stack['Year'] = df_stack['Date_reworked'].dt.year
 
-    # Split column AirlineName on " - " separator
-    df_stack[["DateFromLink", "Country", "Name"]] = df_stack["AirlineName"].str.split(" - ", expand=True)
-
     # Cleaning columns. Dropping non necessary
-    clean_order = ['URL', 'Country', 'Name', 'Last_Date', 'Year', 'Top/Base', 'Salary']
+    clean_order = ['URL', 'Country', 'AirlineName', 'Year', 'Top/Base', 'Salary']
     df_stack = df_stack[clean_order]
 
     df_stack = df_stack.fillna("NAN")
@@ -77,12 +71,9 @@ def merge_jobs(old_jobs, new_jobs):
         old_jobs.append(new_job)
     return old_jobs
 
-
-
-
 if __name__ == "__main__":
     try: 
-        with open("jobs.json", "r") as fd:
+        with open("squawkit_airline_jobs.json", "r") as fd:
             old_jobs = json.load(fd)
     except FileNotFoundError:
         old_jobs = []
@@ -90,5 +81,5 @@ if __name__ == "__main__":
     new_jobs = get_new_jobs(old_jobs)
     jobs = merge_jobs(old_jobs, new_jobs)
 
-    with open("jobs.json", "w") as fd:
+    with open("squawkit_airline_jobs.json", "w") as fd:
         json.dump(jobs, fd)
